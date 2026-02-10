cf88a74e59920398c9af0a5baebc2835a84ec256
mitchcamza <192mitch88@gmail.com>
2026-02-10 00:03:36 -0800

    chore: update HTTPS settings for Render

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -31,9 +31,11 @@ DEBUG = os.getenv("DJANGO_DEBUG")
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS")
 
 CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS").split(",")
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
