1f03352e560504247103ff1cc7deedc66a0e3f80
mitchcamza <192mitch88@gmail.com>
2026-02-10 10:28:02 -0800

    fix: add guard against None type for CSRF_TRUSTED_ORIGINS

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG")
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",") if os.getenv("DJANGO_ALLOWED_HOSTS") else []
-
-CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS").split(",") if os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS") else []
 
 # Application definition
 
