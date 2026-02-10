8a33454d8049be460da17c4356e9cea496b1fcc2
mitchcamza <192mitch88@gmail.com>
2026-02-09 23:51:47 -0800

    fix update CSRF trusted origins
    - add trusted origins in .env file
    - update .env.example

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.getenv("DJANGO_DEBUG")
 ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS").split(",")
 
 # Application definition
 
